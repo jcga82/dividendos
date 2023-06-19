029fdda7438e14b6f9c86fd84112cb2ebc831e1c
Juan Carlos <jcga82@gmail.com>
2023-06-19 09:30:39 +0200

    primer commit

---
+++ Dividendos/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Dividendos
+//
+//  Created by Juan Carlos García Abril on 19/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dividendos/DividendosApp.swift
@@ -0,0 +1,17 @@
+//
+//  DividendosApp.swift
+//  Dividendos
+//
+//  Created by Juan Carlos García Abril on 19/6/23.
+//
+
+import SwiftUI
+
+@main
+struct DividendosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
